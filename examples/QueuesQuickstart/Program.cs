d22be338af5f076b81f5ad23846d6bd8069a11ce
gitName <a@b.c>
2025-05-23 09:28:53 -0600

    Added queue quickstart code

---
+++ examples/QueuesQuickstart/Program.cs
@@ -0,0 +1,84 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
+
+Console.WriteLine("Azure Queue Storage client library - .NET quickstart sample");
+
+// Create a unique name for the queue
+// TODO: Replace the <storage-account-name> placeholder 
+string queueName = "quickstartqueues-" + Guid.NewGuid().ToString();
+string storageAccountName = "stdevidentitymigration";
+
+// Instantiate a QueueClient to create and interact with the queue
+QueueClient queueClient = new QueueClient(
+    new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
+    new DefaultAzureCredential());
+
+Console.WriteLine($"Creating queue: {queueName}");
+
+// Create the queue
+await queueClient.CreateAsync();
+
+Console.WriteLine("\nAdding messages to the queue...");
+
+// Send several messages to the queue
+await queueClient.SendMessageAsync("First message");
+await queueClient.SendMessageAsync("Second message");
+
+// Save the receipt so we can update this message later
+SendReceipt receipt = await queueClient.SendMessageAsync("Third message");
+
+Console.WriteLine("\nPeek at the messages in the queue...");
+
+// Peek at messages in the queue
+PeekedMessage[] peekedMessages = await queueClient.PeekMessagesAsync(maxMessages: 10);
+
+foreach (PeekedMessage peekedMessage in peekedMessages)
+{
+    // Display the message
+    Console.WriteLine($"Message: {peekedMessage.MessageText}");
+}
+
+Console.WriteLine("\nUpdating the third message in the queue...");
+
+// Update a message using the saved receipt from sending the message
+await queueClient.UpdateMessageAsync(receipt.MessageId, receipt.PopReceipt, "Third message has been updated");
+
+QueueProperties properties = queueClient.GetProperties();
+
+// Retrieve the cached approximate message count
+int cachedMessagesCount = properties.ApproximateMessagesCount;
+
+// Display number of messages
+Console.WriteLine($"Number of messages in queue: {cachedMessagesCount}");
+
+Console.WriteLine("\nReceiving messages from the queue...");
+
+// Get messages from the queue
+QueueMessage[] messages = await queueClient.ReceiveMessagesAsync(maxMessages: 10);
+
+Console.WriteLine("\nPress Enter key to 'process' messages and delete them from the queue...");
+Console.ReadLine();
+
+// Process and delete messages from the queue
+foreach (QueueMessage message in messages)
+{
+    // "Process" the message
+    Console.WriteLine($"Message: {message.MessageText}");
+
+    // Let the service know we're finished with
+    // the message and it can be safely deleted.
+    await queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+}
+
+Console.WriteLine("\nPress Enter key to delete the queue...");
+Console.ReadLine();
+
+// Clean up
+Console.WriteLine($"Deleting queue: {queueClient.Name}");
+await queueClient.DeleteAsync();
+
+Console.WriteLine("Done");
